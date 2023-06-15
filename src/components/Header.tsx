1bebb74d9745d2187ede8fed51e6b6b8213d0e15
TatianeMarinho <devtatianemarinho@gmail.com>
2023-06-14 23:57:00 -0400

    02

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import '../styles/Header.css';
 
 export default function Header() {
   return (
     <header>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
       <h2>Bloggers</h2>
     </header>
   );
